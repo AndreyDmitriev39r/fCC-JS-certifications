94654d9502600fd9f01240c4308309281781a8bf
Andrey Dmitriev <admit8490@gmail.com>
2022-07-30 17:14:06 +0200

    handle 1st scenario

--- Cash Register.js
+++ Cash Register.js
@@ -1,9 +1,54 @@
 /*
-input:
-price >>> purchase price
-cash >>> payment
-cid >>> cash-in-drawer
+input: cash-in-drawer
+output: total funds in drawer
 */
-
-function checkCashRegister(price, cash, cid) {
-}
+const countFunds = (cid) => {
+    return cid.reduce((total, currentUnit) => {
+      let sumOfTwo = total + currentUnit[1];
+      return Number(sumOfTwo.toPrecision(3));
+    }, 0
+      );
+  }
+  
+  
+  /*
+  input:
+  price >>> purchase price
+  cash >>> payment
+  cid >>> cash-in-drawer
+  
+  output: Object
+  {
+  status: value(as string)
+  change: value(as array)
+  */
+  function checkCashRegister(price, cash, cid) {
+    
+    let statusAndChange = {
+      status: "INSUFFICIENT_FUNDS", 
+      change: []
+    };
+    
+    const change = cash - price;
+    const totalFunds = countFunds(cid);
+    
+    if (change > totalFunds) {
+      return statusAndChange;
+    }
+  }
+  
+  test1 = checkCashRegister(19.5, 20, 
+  [
+    ["PENNY", 1.01], 
+    ["NICKEL", 2.05], 
+    ["DIME", 3.1], 
+    ["QUARTER", 4.25], 
+    ["ONE", 90], 
+    ["FIVE", 55], 
+    ["TEN", 20], 
+    ["TWENTY", 60], 
+    ["ONE HUNDRED", 100]
+    ]
+    );
+    
+  console.log(test1);

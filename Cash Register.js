501d6cc11f1dd809c255559a66473822cc793dbe
Andrey Dmitriev <admit8490@gmail.com>
2022-07-30 18:32:33 +0200

    final version before fcc tests

--- Cash Register.js
+++ Cash Register.js
@@ -10,6 +10,31 @@ const countFunds = (cid) => {
       );
   }
   
+  const provideChangeInUnits = (change, cid) => {
+    
+    const Nominals = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+    let changeInUnits = [];
+    
+    for (let index = Nominals.length - 1; index >= 0; index--) {
+      
+      let currNom = Nominals[index];
+      
+      if (change >= currNom) {
+        let valueToProvide = currNom * Math.floor(change / currNom);
+        changeInUnits.unshift(
+              [cid[index][0], valueToProvide]
+              );
+        change = (change - valueToProvide).toPrecision(3);
+      }
+      
+      if (change == 0) {break;}
+      
+    }
+    
+    return [changeInUnits, change];
+  }
+  
+  
   
   /*
   input:
@@ -41,6 +66,16 @@ const countFunds = (cid) => {
       statusAndChange.change = cid;
       return statusAndChange;
     }
+    
+    const changeInUnits = provideChangeInUnits(change, cid);
+    
+    if (changeInUnits[1] !== '0.00') {
+      return statusAndChange;
+    }
+    
+    statusAndChange.status = "OPEN";
+    statusAndChange.change = changeInUnits[0]
+    return statusAndChange;
   }
   
   test1 = checkCashRegister(19.5, 20, 
@@ -56,3 +91,12 @@ const countFunds = (cid) => {
     ["ONE HUNDRED", 100]
     ]
     );
+  
+  test2 = checkCashRegister(19.5, 20, 
+  [
+    ["PENNY", 0.5], ["NICKEL", 0], 
+    ["DIME", 0], ["QUARTER", 0],
+    ["ONE", 0], ["FIVE", 0], ["TEN", 0], 
+    ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  
+  console.log(test1);
